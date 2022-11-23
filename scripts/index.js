b372c948a3ccc9f81bd74de05064edd428d4ece4
Andrey Daskal <andreydaskal2002@gmail.com>
2022-11-23 22:37:22 +0300

    Create template element

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ const inputStatus = popupEdit.querySelector(".popup__input_status");
 const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
 const popupForm = document.querySelector(".popup__form");
+const initialItem = ["Программировать", "Покурить", "Поесть", "Поиграть"]
+const todo = document.querySelector(".todo")
 // Функции
 function openPopup(popup) {
   popup.classList.add("popup_is-opened");
@@ -32,3 +34,20 @@ closeButton.addEventListener("click", () => {
   closePopup(popupEdit);
 });
 popupForm.addEventListener("submit", formSubmitHandler);
+
+function renderItem(text) {
+  const template = document.querySelector("#template").content
+
+  const listItem = template.cloneNode(true)
+  const textElement = listItem.querySelector(".todo__text")
+  textElement.textContent = text
+  todo.append(listItem)
+  console.log(textElement)
+}
+
+function renderList(array) {
+  array.forEach(element => {
+    renderItem(element)
+  });
+}
+renderList(initialItem)
